2127230e002f4d1b2d33118324c02a033dff11e0
zm <2767272859@qq.com>
2025-08-05 19:59:56 +0800

    two factory test

--- src/com/main.java
+++ src/com/main.java
@@ -2,6 +2,8 @@ package com;
 
 import arc.*;
 import arc.util.*;
+import com.blocks.factory.KSImpactor;
+import com.blocks.factory.sublimator;
 import com.items.items;
 import com.liquids.liquids;
 import mindustry.game.EventType.*;
@@ -31,6 +33,17 @@ public class main extends Mod{
     public void loadContent(){
         items.load();
         liquids.load();
+
+        new sublimator("sublimator"){{
+            localizedName = "升华器";
+            description = "蓄能完毕后，产生诅咒黄金————咒金";
+        }};
+
+        new KSImpactor("KSImpactor"){{
+            localizedName = "KS冲击器";
+            description = "把KS1688457强行冲进‘升化器’内";
+            details = "这种鲁莽的手段看起来非常不可靠，但是据百年来的炼金史来看......他应该不会产生什么不可逆的后果。\n但愿吧";
+        }};
     }
 
 }
